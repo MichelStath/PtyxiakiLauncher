3cd12dbcf2f2c48e40e9a8a15fe5e422c161e8e0
Michalis <57771939+MichelStath@users.noreply.github.com>
2023-03-29 23:58:39 +0300

    Homepage layout

--- app/src/main/java/com/example/activities/ptyxiakilauncher/MainActivity.java
+++ app/src/main/java/com/example/activities/ptyxiakilauncher/MainActivity.java
@@ -1,33 +1,149 @@
 package com.example.activities.ptyxiakilauncher;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.provider.Browser;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
-    TextView batteryStatus;
+    TextView batteryLevelTV, dateTV, timeTV;
+    ImageView batteryLevelIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        batteryLevelTV = findViewById(R.id.batteryLevelTV);
+        dateTV = findViewById(R.id.dateTV);
+        timeTV = findViewById(R.id.timeTV);
+        batteryLevelIV = findViewById(R.id.batteryLevelIV);
+        setDateTime();
+        //this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){ // MUST NEW THREAD
+        @Override
+        public void onReceive(Context ctx, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            int batteryPct = (int) (level * 100 / (float)scale);
+            //setBatteryLevel(batteryPct);
+
+        }
+    };
 
 
 
 
+    public void setBatteryLevel(int level){
+        batteryLevelTV.setText(String.valueOf(level) + " %");
+        if (level == 100) battLevelFull();
+        if(level > 95) battLevel6();
+        else if (level > 85) battLevel5();
+        else if (level > 60) battLevel4();
+        else if (level > 45) battLevel3();
+        else if (level > 30 ) battLevel2();
+        else battLevel1();
+    }
 
+    public void setDateTime(){
+        String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+        dateTV.setText(currentDate);
+        timeTV.setText(currentTime);
+    }
 
-
-
+    //region visualize battery level
+    public void battLevel1(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.red));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_1_bar_24));
+    }
+    public void battLevel2(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.orangered));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_2_bar_24));
+    }
+    public void battLevel3(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.orange));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_3_bar_24));
+    }
+    public void battLevel4(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.olive));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_4_bar_24));
+    }
+    public void battLevel5(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.green));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_5_bar_24));
+    }
+    public void battLevel6(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.lightgreen));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_6_bar_24));
+    }
+    public void battLevelFull(){
+        batteryLevelTV.setTextColor(ContextCompat.getColor(getApplicationContext(),R.color.lightgreen));
+        batteryLevelIV.setForeground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.baseline_battery_full_24));
+    }
+    //endregion
 
 
     public void testBTN(View view) {
         Intent i = new Intent(this,InfoActivity.class);
         startActivity(i);
     }
+
+    //region HOME BUTTONS
+    public void dialBTN_Clicked(View view) {
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    public void contactsBTN_Clicked(View view) {
+        Intent i = new Intent(Browser.EXTRA_CREATE_NEW_TAB);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //startActivity(i);
+    }
+
+    public void messageBTN_Clicked(View view) {
+    }
+
+    public void infoBTN_Clicked(View view) {
+        Intent i = new Intent(this,InfoActivity.class);
+        startActivity(i);
+    }
+
+    public void sosBTN_Clicked(View view) {
+    }
+
+    public void allAppsBTN_Clicked(View view) {
+    }
+
+    public void settingsBTN_Clicked(View view) {
+        Intent i = new Intent(android.provider.Settings.ACTION_SETTINGS);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+    //endregion
+
+
+
+
+
+
 }
